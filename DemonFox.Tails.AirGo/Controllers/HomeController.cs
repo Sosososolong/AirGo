9a7c5a2a68eca741fc816024ae6367a1186fdae0
sosososolong <849596984@qq.com>
2019-09-06 16:13:33 +0800

    亿元后台代码生成页面

--- DemonFox.Tails.AirGo/Controllers/HomeController.cs
+++ DemonFox.Tails.AirGo/Controllers/HomeController.cs
@@ -15,6 +15,10 @@ namespace DemonFox.Tails.AirGo.Controllers
     public class HomeController : Controller
     {
         private DemonProvider db = new DemonProvider();
+        /// <summary>
+        /// 需要的参数："sql", "connectionString", 
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Index()
         {
             //准备所需要的数据
@@ -213,6 +217,270 @@ namespace DemonFox.Tails.AirGo.Controllers
 
         }
     }").Append(newLine).Append(newLine);
+            return Content(builder.ToString(), "text/plain", Encoding.UTF8);
+        }
+
+        public IActionResult GameBackend()
+        {
+            string sql = Request.Form["sql"];
+            string connectionString = DemonProvider.ConnectionString = Request.Form["connectionString"];
+            //string sql = Request.Query["sql"];
+            //string connectionString = DemonProvider.ConnectionString = Request.Query["connectionString"];
+
+            if (sql.IndexOf("@pageIndex") == 0 || sql.IndexOf("@pageSize") == 0)
+            {
+                return Content($"sql语句: {sql}中, 没有@pageIndex和@pageSize参数");
+            }
+            DbParameter[] parameters = new DbParameter[2]
+            {
+                db.CreateDbParameter("pageIndex", 1),
+                db.CreateDbParameter("pageSize", 10)
+            };
+            DataSet set = db.ExecuteQuerySql(sql, parameters);
+            DataTable dataTable = set.Tables[0];
+            List<string> columnNames = new List<string>();
+            foreach (DataColumn column in dataTable.Columns)
+            {
+                columnNames.Add(column.ColumnName);
+            }
+
+            string newLine = Environment.NewLine;
+            StringBuilder builder = new StringBuilder();
+            builder.Append(@"    <title></title>
+    <link href=""/styles/layout.css"" rel=""stylesheet"" type=""text/css"" />
+    <script src = ""/Scripts/comm.js"" type = ""text/javascript""></ script>
+    <script src = ""/Scripts/common.js"" type = ""text/javascript""></ script>
+    <script type = ""text/javascript"" src = ""/scripts/My97DatePicker/WdatePicker.js""></ script>
+    <style type = ""text/css"">
+        .hidden{ display: none; }
+        .greenColor{ color: green; }
+        a{ text - decoration:none; }
+        a: hover{ text - decoration:none}
+    </ style> ").Append(newLine).Append(newLine)
+                .Append("----------------------------------------Body---------------------------------------").Append(newLine).Append(newLine)
+                .Append(@"    <form id=""form1"" runat=""server"">
+        <!-- 头部菜单 Start -->
+        <table width=""100%"" border=""0"" cellpadding=""0"" cellspacing=""0"" class=""title"">
+            <tr>
+                <td width=""19"" height=""25"" valign=""center"" class=""Lpd10"">
+                    <div class=""arr"">
+                    </div>
+                </td>
+                <td width=""1232"" height=""25"" valign=""center"" align=""left"">
+                    你当前位置：客服中心 - XXXX
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    玩家返现统计：<asp:Label runat=""server"" ID=""AllCashback"" CssClass=""greenColor""></asp:Label>
+                </td>
+               
+            </tr>
+        </table>
+        <!-- 头部菜单 End -->
+        <table width=""100%"" border=""0"" align=""center"" cellpadding=""0"" cellspacing=""0"" class=""titleQueBg"">
+            <tr>
+                <td class=""listTdLeft"" style=""width: 80px"">玩家编号：</td>
+                <td>
+                    <asp:TextBox ID=""txtNickName"" runat=""server"" CssClass=""text""></asp:TextBox>
+                    &nbsp;&nbsp;申请时间：<asp:TextBox ID=""txtStartDate"" runat=""server"" CssClass=""text"" Width=""80px"" onfocus=""WdatePicker({dateFmt:'yyyy-MM-dd',maxDate:'#F{$dp.$D(\'txtEndDate\')}'})""></asp:TextBox>
+                    至 
+                <asp:TextBox ID=""txtEndDate"" runat=""server"" CssClass=""text"" Width=""80px"" onfocus=""WdatePicker({dateFmt:'yyyy-MM-dd',minDate:'#F{$dp.$D(\'txtStartDate\')}'})""></asp:TextBox>
+                    &nbsp;&nbsp; <%--审核状态：
+                    <asp:DropDownList ID=""ddlSearchType"" runat=""server"">
+                        <asp:ListItem Value=""3"">全部</asp:ListItem>
+                        <asp:ListItem Value=""0"">正在审核</asp:ListItem>
+                        <asp:ListItem Value=""1"">已通过</asp:ListItem>
+                        <asp:ListItem Value=""2"">已驳回</asp:ListItem>                        
+                    </asp:DropDownList>--%>                    
+                    <asp:Button ID=""btnSearch"" runat=""server"" Text=""搜索"" CssClass=""wd2 btn"" OnClick=""btnSearch_Click"" />                   
+                </td>
+            </tr>
+        </table>
+        <div id=""content"">           
+            <asp:ScriptManager ID=""ScriptManager1"" runat=""server"">
+            </asp:ScriptManager>
+            <asp:UpdatePanel ID = ""UpdatePanel1"" runat = ""server"" UpdateMode = ""Conditional"" >
+                <ContentTemplate>
+                    <table width = ""97%"" border = ""0"" align = ""center"" cellpadding = ""0"" cellspacing = ""0"" class=""box Tmg7"" id=""list"">
+                        <tr align = ""center"" class=""bold"">").Append(newLine);
+
+            //拼接表格所有的列
+            foreach (string columnName in columnNames)
+            {
+                builder.AppendFormat(@"                            <th class=""need-order"" field_name=""{0}"">{0}</th>", columnName).Append(newLine);
+            }
+
+            builder.Append(@"                        </tr>
+                        <asp:Repeater ID=""rptUserList"" runat=""server"">
+                            <ItemTemplate >
+                                <tr align = ""center"" class=""list"" onmouseover=""currentcolor=this.style.backgroundColor;this.style.backgroundColor='#caebfc';this.style.cursor='default';""
+                                    onmouseout=""this.style.backgroundColor=currentcolor"">").Append(newLine);
+
+            foreach (string columnName in columnNames)
+            {
+                builder.Append($@"                                    <td>
+                                        <%# Eval(""{columnName}"") %>
+                                    </td> ").Append(newLine);
+            }
+
+            builder.Append(@"                                </tr>
+                            </ItemTemplate>
+                            <AlternatingItemTemplate>
+                                <tr align=""center"" class=""listBg"" onmouseover=""currentcolor = this.style.backgroundColor; this.style.backgroundColor = '#caebfc'; this.style.cursor = 'default'; ""
+                                    onmouseout = ""this.style.backgroundColor=currentcolor"">").Append(newLine);
+
+            foreach (string columnName in columnNames)
+            {
+                builder.Append($@"                                    <td>
+                                        <%# Eval(""{columnName}"") %>
+                                    </td>").Append(newLine);
+            }
+
+            builder.Append(@"                                </tr>
+                            </AlternatingItemTemplate>
+                        </asp:Repeater>
+                        <asp:Literal runat=""server"" ID=""litNoData"" Visible=""true"" Text="" < tr class='tdbg'><td colspan = '100' align='center'><br>没有任何信息!<br><br></td></tr>""></asp:Literal>
+                        <asp:Button runat = ""server"" ID =""ReOrderBtn"" CssClass =""hidden"" OnClick =""ReOrderBtn_Click"" />
+                        <asp:HiddenField runat = ""server"" ID = ""OrderFieldName"" ClientIDMode = ""Static"" />
+                    </table>
+
+                    <table width = ""100%"" border=""0"" align=""center"" cellpadding=""0"" cellspacing=""0"">
+                        <tr>
+                            <td align = ""right"" class=""page"">
+                                <webdiyer:AspNetPager UrlPaging = ""false"" ID=""anpUserList"" runat=""server"" OnPageChanged=""anpInsure_PageChanged"" AlwaysShow=""false"" FirstPageText=""首页"" LastPageText=""末页""
+                                    PageSize=""14"" NextPageText=""下页"" PrevPageText=""上页"" ShowBoxThreshold=""0"" ShowCustomInfoSection=""Left"" LayoutType=""Table""
+                                    NumericButtonCount=""5"" CustomInfoHTML=""总记录：%RecordCount%　页码：%CurrentPageIndex%/%PageCount%　每页：%PageSize%"">
+                                </webdiyer:AspNetPager>
+                            </td>
+                        </tr>
+                    </table>
+                </ContentTemplate>
+            </asp:UpdatePanel>
+        </div>        
+    </form>
+    <script>
+        //updatepanel请求结束后触发
+        var prm = Sys.WebForms.PageRequestManager.getInstance();
+        prm.add_endRequest(function () {
+            //异步刷新后也要执行 ""给列头绑定click事件的代码"", 否则异步刷新后""th""的click事件失效
+            bindOrder();
+    });
+        function bindOrder()
+    {
+            $(""th"").click(function() {
+            var fieldName = $(this).attr(""field_name"")
+                if (fieldName.length > 0)
+            {
+                OrderFieldName.value = fieldName;
+                ReOrderBtn.click();
+            }
+        });
+    }
+    bindOrder();                
+    </script>").Append(newLine).Append(newLine)
+    .Append("----------------------------------------Code---------------------------------------").Append(newLine).Append(newLine)
+    .Append(@"        private static string _orderByField = ""record.RecordID"";
+        private static bool _isAsc = false;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindData();
+            }
+        }
+
+        private void BindData()
+        {
+            string searchStr = """";
+            //1. 用户编号
+            string gameId = txtNickName.Text;
+            if (!string.IsNullOrEmpty(gameId))
+            {
+                searchStr += "" and account.GameID like '%"" + gameId + ""%' "";
+            }
+            //2. 时间
+            string startTimeStr = string.IsNullOrEmpty(txtStartDate.Text) ? ""1970-01-01"" : txtStartDate.Text;
+            string endTimeStr = string.IsNullOrEmpty(txtEndDate.Text) ? DateTime.Now.AddDays(1).ToString(""yyyy-MM-dd"") : Convert.ToDateTime(txtEndDate.Text).AddDays(1).ToString(""yyyy-MM-dd"");
+            //当开始时间晚于结束时间，对调
+            if (DateTime.Compare(Convert.ToDateTime(startTimeStr), Convert.ToDateTime(endTimeStr)) > 0)
+            {
+                string temp = startTimeStr;
+                startTimeStr = endTimeStr;
+                endTimeStr = temp;
+            }
+            searchStr += $"" and record.CollectDate between '{startTimeStr}' and '{endTimeStr}'"";
+            //3. 是否通过, 0：正在审核   1：通过    2：驳回    3.显示全部信息
+            //int status = Convert.ToInt32(ddlSearchType.SelectedValue);
+            //if (status != 3)
+            //{
+            //    searchStr += $"" and records.RebateStatus = {status}"";
+            //}
+
+            //获取数据
+            int pageIndex = anpUserList.CurrentPageIndex;
+
+            int pageSize = 14;
+
+            DataSet dataSet;
+            string orderByDirection = _isAsc ? ""ASC"" : ""DESC"";
+            if (string.IsNullOrEmpty(_orderByField))
+            {
+                dataSet = aideGUser.GetTransferList(pageIndex, pageSize, _orderByField, orderByDirection, searchStr);
+            }
+            else
+            {
+                dataSet = aideGUser.GetTransferList(pageIndex, pageSize, _orderByField, orderByDirection, searchStr);
+            }
+
+
+            if (dataSet.Tables[0].Rows.Count > 0)
+            {
+                litNoData.Visible = false;
+            }
+            else
+            {
+                litNoData.Visible = true;
+            }
+
+            rptUserList.DataSource = dataSet.Tables[0];
+            rptUserList.DataBind();
+            anpUserList.RecordCount = Convert.ToInt32(dataSet.Tables[1].Rows[0][0]);
+
+            //所有玩家的 返现请求总和
+            AllCashback.Text = !string.IsNullOrEmpty(dataSet.Tables[1].Rows[0][""AllSwap""].ToString()) ?
+                TextUtility.FormatMoney(Convert.ToDecimal(dataSet.Tables[1].Rows[0][""AllSwap""].ToString()) / pic) :
+                ""0.00"";
+        }        
+
+        protected void anpInsure_PageChanged(object sender, EventArgs e)
+        {
+            BindData();
+        }
+        /// <summary>
+        /// 搜索
+        /// </summary>
+        /// <param name=""sender""></param>
+        /// <param name=""e""></param>
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            //搜索显示第一页信息（数据少的时候只有一页信息）
+            anpUserList.CurrentPageIndex = 1;
+            BindData();
+        }
+        /// <summary>
+        /// 排序
+        /// </summary>
+        /// <param name=""sender""></param>
+        /// <param name=""e""></param>
+        protected void ReOrderBtn_Click(object sender, EventArgs e)
+        {
+            anpUserList.CurrentPageIndex = 1;
+            if (!string.IsNullOrEmpty(OrderFieldName.Value))
+            {
+                _orderByField = OrderFieldName.Value == ""RowNumber"" ? ""Id"" : OrderFieldName.Value;
+            }
+
+            _isAsc = !_isAsc;
+            BindData();
+        }").Append(newLine).Append(newLine);
             return Content(builder.ToString(), "text/plain", Encoding.UTF8);
         }
 
---
+++ DemonFox.Tails.Utils/HttpHelper.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemonFox.Tails.Utils
+{
+    public class HttpHelper
+    {
+        /// <summary>
+        /// 发送一个Http Get请求，返回字符串结果
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static async Task<string> HttpGetAsync(string url)
+        {
+            using (var http = new HttpClient())
+            {
+                //await异步等待回应
+                var response = await http.GetAsync(url);
+                //确保HTTP成功状态值
+                response.EnsureSuccessStatusCode();
+
+                //await异步读取最后的JSON（注意此时gzip已经被自动解压缩了，因为上面的AutomaticDecompression = DecompressionMethods.GZip）
+                return await response.Content.ReadAsStringAsync();
+            }
+        }   
+        
+        /// <summary>
+        /// 发送一个Http Post请求，返回字符串结果
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="dic"></param>
+        /// <returns></returns>
+        public static async Task<string> HttpPostAsync(string url, IDictionary<string, string> dic)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                FormUrlEncodedContent content = null;
+                if (dic != null)
+                {
+                    content = new FormUrlEncodedContent(dic);
+                }
+                var response = await client.PostAsync(url, content);
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+
+        /// <summary>
+        /// AES加密
+        /// </summary>
+        /// <param name="toEncrypt">加密串</param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string AESEncrypt(string toEncrypt, string key)
+        {
+            byte[] resultArray = Encrypt_byte(toEncrypt, key);
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+
+        /// <summary>
+        /// AES加密
+        /// </summary>
+        /// <param name="toEncrypt">加密串</param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        private static byte[] Encrypt_byte(string toEncrypt, string key)
+        {
+            byte[] keyArray = Encoding.UTF8.GetBytes(key);
+            byte[] toEncryptArray = Encoding.UTF8.GetBytes(toEncrypt);
+            RijndaelManaged rDel = new RijndaelManaged();
+            rDel.Key = keyArray;
+            rDel.Mode = CipherMode.ECB;
+            rDel.Padding = PaddingMode.PKCS7;
+            ICryptoTransform cTransform = rDel.CreateEncryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+            //return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+            return resultArray;
+        }
+
+
+        /// <summary>
+        ///  AES解密
+        /// </summary>
+        /// <param name="toDecrypt"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string AESDecrypt(string toDecrypt, string key)
+        {
+            byte[] keyArray = Encoding.UTF8.GetBytes(key);
+            byte[] toEncryptArray = Convert.FromBase64String(toDecrypt);
+            RijndaelManaged rDel = new RijndaelManaged();
+            rDel.Key = keyArray;
+            rDel.Mode = CipherMode.ECB;
+            rDel.Padding = PaddingMode.PKCS7;
+            ICryptoTransform cTransform = rDel.CreateDecryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+            return Encoding.UTF8.GetString(resultArray);
+        }
+
+        /// <summary>
+        /// 获取客户端IP 适用于.net mvc
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        //public static string GetClientIP(HttpContext context)
+        //{
+
+        //    string text = "0.0.0.0";
+        //    string text2 = text;
+        //    if (context == null)
+        //    {
+        //        return text2;
+        //    }
+        //    text2 = context.Request.ServerVariables["X_Forwarded_For"];
+        //    if (string.IsNullOrEmpty(text2) || string.Compare("unknown", text2, true) == 0)
+        //    {
+        //        text2 = context.Request.ServerVariables["Proxy-Client-IP"];
+        //    }
+        //    if (string.IsNullOrEmpty(text2) || string.Compare("unknown", text2, true) == 0)
+        //    {
+        //        text2 = context.Request.ServerVariables["WL-Proxy-Client-IP"];
+        //    }
+        //    if (string.IsNullOrEmpty(text2) || string.Compare("unknown", text2, true) == 0)
+        //    {
+        //        text2 = context.Request.ServerVariables["HTTP_CLIENT_IP"];
+        //    }
+        //    if (string.IsNullOrEmpty(text2) || string.Compare("unknown", text2, true) == 0)
+        //    {
+        //        text2 = context.Request.ServerVariables["HTTP_VIA"];
+        //    }
+        //    if (string.IsNullOrEmpty(text2) || string.Compare("unknown", text2, true) == 0)
+        //    {
+        //        text2 = context.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+        //    }
+        //    if (string.IsNullOrEmpty(text2) || string.Compare("unknown", text2, true) == 0)
+        //    {
+        //        text2 = context.Request.ServerVariables["Remote_Addr"];
+        //    }
+        //    if (text2 == "::1")
+        //    {
+        //        text2 = "127.0.0.1";
+        //    }            
+        //    if (text2 != text)
+        //    {
+        //        text2 = text2.Replace(" ", "");
+        //        if (text2.IndexOf(",") > 0)
+        //        {
+        //            text2 = text2.Split(new char[]
+        //            {
+        //                ','
+        //            })[0];
+        //        }
+        //    }
+        //    return text2;
+
+        //}
+    }
+}
