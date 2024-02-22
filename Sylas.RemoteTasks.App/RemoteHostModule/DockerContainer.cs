94af447ac22315ebbf0a3860fcba5a5e7123e8ff
wql <849596984@qq.com>
2024-02-22 10:43:19 +0800

    style:为RemoteHost相关类添加注释

--- Sylas.RemoteTasks.App/RemoteHostModule/DockerContainer.cs
+++ Sylas.RemoteTasks.App/RemoteHostModule/DockerContainer.cs
@@ -1,18 +1,45 @@
 ﻿namespace Sylas.RemoteTasks.App.RemoteHostModule
 {
+    /// <summary>
+    /// 服务器信息 - 容器
+    /// </summary>
     public class DockerContainer : RemoteHostInfo
     {
+        /// <summary>
+        /// 容器Id
+        /// </summary>
         public string ContainerId { get; set; } = string.Empty;
+        /// <summary>
+        /// 容器的镜像
+        /// </summary>
         public string Image { get; set; } = string.Empty;
+        /// <summary>
+        /// 容器的后台程序
+        /// </summary>
         public string Command { get; set; } = string.Empty;
+        /// <summary>
+        /// 容器创建信息
+        /// </summary>
         public string Created { get; set; } = string.Empty;
+        /// <summary>
+        /// 运行状态
+        /// </summary>
         public string Status { get; set; } = string.Empty;
+        /// <summary>
+        /// 端口信息
+        /// </summary>
         public string Ports { get; set; } = string.Empty;
         /// <summary>
         /// 用容器名称表示
         /// </summary>
         public string Names { get; set; } = string.Empty;
+        /// <summary>
+        /// 别名
+        /// </summary>
         public override string ShortName => ShortNameMapDictionary.TryGetValue(Name, out string? shortName) ? shortName : Name;
+        /// <summary>
+        /// 容器名与别名的映射关系
+        /// </summary>
         public override Dictionary<string, string> ShortNameMapDictionary => new()
             {
                 { "iduo.ids4", "ids4" },
@@ -25,12 +52,17 @@
                 { "iduo.engine", "engine" },
                 { "iduo.bpmclient", "bpm" },
             };
+        /// <summary>
+        /// 描述
+        /// </summary>
         public override string Description { get => $"Container: {Names} [{ContainerId}] created from Image: {Image} at {Created}"; }
         /// <summary>
         /// 用容器名称表示
         /// </summary>
         public override string Name => Names;
-
+        /// <summary>
+        /// 标签列表, 用于展示
+        /// </summary>
         public override List<Tuple<string, string>> Labels
         {
             get
--- Sylas.RemoteTasks.App/RemoteHostModule/RemoteHostInfo.cs
+++ Sylas.RemoteTasks.App/RemoteHostModule/RemoteHostInfo.cs
@@ -1,9 +1,21 @@
 ﻿namespace Sylas.RemoteTasks.App.RemoteHostModule
 {
+    /// <summary>
+    /// 主机信息
+    /// </summary>
     public abstract class RemoteHostInfo
     {
+        /// <summary>
+        /// 描述
+        /// </summary>
         public abstract string Description { get; }
+        /// <summary>
+        /// 主机信息名称
+        /// </summary>
         public abstract string Name { get; }
+        /// <summary>
+        /// 主机信息别名
+        /// </summary>
         public abstract string ShortName {  get; }
         /// <summary>
         /// 有时候一个服务器信息名称可能有简写, 名称与名称简写之间的映射字典
@@ -11,8 +23,13 @@
         /// <param name="name"></param>
         /// <returns></returns>
         public abstract Dictionary<string, string> ShortNameMapDictionary {  get; }
+        /// <summary>
+        /// 主机信息标签列表, 用于展示主机信息展示
+        /// </summary>
         public abstract List<Tuple<string, string>> Labels { get; }
+        /// <summary>
+        /// 对当前主机信息的操作集合
+        /// </summary>
         public virtual List<CommandInfo> Commands { get; set; } = [];
-        public virtual List<CommandInfo> HostInfoCommands { get; set; } = [];
     }
 }
